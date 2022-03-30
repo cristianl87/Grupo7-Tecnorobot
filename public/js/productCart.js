97bbb551858507a1671da7dbbb8d4d478779ecd2
Ariel Dorssi <adorssi@gmail.com>
2022-03-30 13:52:29 -0300

    Funcionalidades en pagina carrito

--- public/js/productCart.js
+++ public/js/productCart.js
@@ -1,8 +1,16 @@
-const cart = JSON.parse((localStorage.getItem('tecnoCart')));
+let cart = JSON.parse((localStorage.getItem('tecnoCart')));
 const productsContainer = document.getElementById('products-container');
+const articulosCarrito = document.querySelector('.articulo-carrito');
+const totalCarrito = cart.reduce((total, product) => total + parseInt(product.price), 0);
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    cart.forEach(product => {
+    if(cart == null || cart.length == 0) {
+        productsContainer.innerHTML = `<h2 class="noProducts">No hay productos en el carrito <a class="toStore" href="/products">Ir a la Tienda <i class="fas fa-store-alt"></i> </a> </h2>`;
+    } else {
+
+        cart.forEach(product => {
+
         const divProducto = document.createElement('div');
         divProducto.innerHTML = `
         <div class="producto-carrito">
@@ -12,19 +20,38 @@ document.addEventListener('DOMContentLoaded', () => {
                 <p class="precio-unitario">${product.currency.symbol} ${product.price}</p>
                 <div class="cantidad-producto">
                     <div class="recuadro-cantidad">
-                        <div class="texto-recuadro" id="menos-mas">-</div>
-                        <div class="texto-recuadro">1</div>
-                        <div class="texto-recuadro" id="menos-mas">+</div>
+                    <form>
+                        <input type="number" id="quantity" value="1" min="1" max="10" onClick="recalcularSubtotal(this)">
+                    </form>
                     </div>
                 </div>
-                <p class="subtotal">$200</p>
+                <p class="subtotal"></p>
             </div>
             <div class="tacho-basura">
-                <i class="far fa-trash-alt" id="tachito"></i>
+                <i class="far fa-trash-alt delete" id="tachito" data-id="${product.id}"></i>
             </div>
         </div>
         `
         productsContainer.appendChild(divProducto);
     });
+    }
+    articulosCarrito.addEventListener('click', deleteProduct);
 })
 
+function deleteProduct(e) {
+    if(e.target.classList.contains('delete')) {
+        const id = e.target.dataset.id;
+        const newCart = cart.filter(product => product.id != id);
+        console.log(newCart);
+        localStorage.setItem('tecnoCart', JSON.stringify(newCart));
+        location.reload();
+    }
+    
+}
+
+function recalcularSubtotal(e) {
+    const cantidad = e.value;
+    // const precio = e.target.parentElement.parentElement.querySelector('.precio-unitario').textContent;
+    // const subtotal = cantidad * precio;
+    // e.target.parentElement.parentElement.querySelector('.subtotal').textContent = subtotal;
+}
